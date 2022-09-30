83093e959339ab676631bc3570b9bb4a01c4ff8f
benjamin-smit <bcsmithaz@gmail.com>
2022-09-29 18:16:51 -0700

    finished

---
+++ src/main/java/com/csc205/project2/Shape.java
@@ -0,0 +1,18 @@
+package com.csc205.project2;
+
+public class Shape implements ThreeDimensionalShape {
+
+    public Shape(){
+
+    }
+
+    @Override
+    public double surfaceArea() {
+        return surfaceArea();
+    }
+
+    @Override
+    public double volume() {
+        return volume();
+    }
+}
